cd803a8fe651efb41c7474cbb3c8470284bd674e
shawonahmed1 <mdshawon356@gmail.com>
2020-09-13 00:26:09 +0600

    after google sign in

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,37 @@
 import React, { useContext } from 'react';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
 
 
 const Login = () => {
+
+    firebase.initializeApp(firebaseConfig);
+    const handleGoogleSignIn = () => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            console.log(user);
+            // ...
+          }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+          });
+    }
     return (
         <div>
             <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn}>Google Sign In</button>
         </div>
     );
 };
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB-wS8BADnbctMm2j7fdMQXfPAnxlcnBc0",
+    authDomain: "burje-al-arab-project-website.firebaseapp.com",
+    databaseURL: "https://burje-al-arab-project-website.firebaseio.com",
+    projectId: "burje-al-arab-project-website",
+    storageBucket: "burje-al-arab-project-website.appspot.com",
+    messagingSenderId: "932966327272",
+    appId: "1:932966327272:web:3a921fab4f4c23ed9a3a5d"
+  };
+  export default firebaseConfig;
